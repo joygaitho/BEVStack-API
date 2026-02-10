9d1377152b92749b4815e45c8b54af58d11babaf
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2026-02-10 21:55:02 +0300

    feat: allow public read access to categories

--- drinks_api/core/views.py
+++ drinks_api/core/views.py
@@ -21,8 +21,11 @@ from .serializers import CategorySerializer
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+    
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
