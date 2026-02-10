f585fdfffc7a0d7fc043d35879b6adafaff88a6f
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2026-02-10 18:50:09 +0300

    feat: expose category API endpoints

--- drinks_api/core/urls.py
+++ drinks_api/core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import health_check
+from .views import (
+    health_check,
+    CategoryListCreateView,
+    CategoryDetailView,
+) 
 
 urlpatterns = [
     path('health/', health_check, name='health_check'),
+    path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
 ]
