df9ba6b6f09b1f649626afd57e3e200b5091a4eb
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2026-02-10 18:36:05 +0300

    feat: add API versioning and custom exception handling

--- drinks_api/drinks_api/settings.py
+++ drinks_api/drinks_api/settings.py
@@ -139,6 +139,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
+    'EXCEPTION_HANDLER': 'core.exceptions.custom_exception_handler',
 }
 
 LOGGING = {
