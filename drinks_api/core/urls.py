4d7e617e397e4c832fe99c9f344bc12baf9980c3
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2026-02-11 13:52:04 +0300

    feat: expose drink API endpoints

--- drinks_api/core/urls.py
+++ drinks_api/core/urls.py
@@ -3,10 +3,15 @@ from .views import (
     health_check,
     CategoryListCreateView,
     CategoryDetailView,
+    DrinkListCreateView,
+    DrinkDetailView,
 ) 
 
 urlpatterns = [
     path('health/', health_check, name='health_check'),
     path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
     path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+    path('drinks/', DrinkListCreateView.as_view(), name='drink-list-create'),
+    path('drinks/<int:pk>/', DrinkDetailView.as_view(), name='drink-detail'),
+
 ]
