fd57f7668820a30f3760778591e23e2093ce4b79
Joy Wanjiru Gaitho <cirujoygaitho7103@gmail.com>
2026-02-17 12:42:28 +0300

    feat: delete serializers and its contents

--- drinks_api/core/serializers/category.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models import Category
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'description', 'created_at']
-        read_only_fields = ['id', 'created_at']
